9932a89fd1047f21e190e602455f5e60ddaa7ca0
Victoria Truong <victoriataitruong@gmail.com>
2025-03-16 20:56:19 -0700

    port 5001 to 5000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // Enable CORS with specific configuration
 app.use(cors({
